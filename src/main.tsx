0aaad2a5251f09c4cdec680c8d43aa62e73f7b9c
shovalfransis <shovalfransis@gmail.com>
2026-06-13 11:25:42 +0300

    Refactor code structure for improved readability and maintainability

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import App from './App';
 import { StockDetailWrapper } from './components/StockDetailWrapper';
 import './index.css';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+  return null;
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/stock/:symbol" element={<StockDetailWrapper />} />
